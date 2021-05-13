fcbe6497d94c445ad90020a55eed14db17735dbc
phschiele <phschiele@gmail.com>
2021-05-13 17:49:48 +0200

    Activate

--- continuous_integration/GitHub/install_dependencies.sh
+++ continuous_integration/GitHub/install_dependencies.sh
@@ -12,15 +12,18 @@ fi
 
 if [[ "$PYTHON_VERSION" == "3.7" ]]; then
   conda install mkl pip pytest scipy=1.1 numpy=1.15 lapack ecos scs osqp flake8 cvxopt
+  conda activate test
 elif [[ "$PYTHON_VERSION" == "3.8" ]]; then
   # There is a config that works with numpy 1.14, but not 1.15!
   # So we fix things at 1.16.
   # Assuming we use numpy 1.16, the earliest version of scipy we can use is 1.3.
   conda install mkl pip pytest scipy=1.3 numpy=1.16 lapack ecos scs osqp flake8 cvxopt
+  conda activate test
 elif [[ "$PYTHON_VERSION" == "3.9" ]]; then
   # The earliest version of numpy that works is 1.19.
   # Given numpy 1.19, the earliest version of scipy we can use is 1.5.
   conda install mkl pip pytest scipy=1.5 numpy=1.19 lapack ecos scs flake8 cvxopt
+  conda activate test
   pip install osqp
 fi
 
