f2298dc6a03c8c899f6abc639acd63651a2569c6
phschiele <phschiele@gmail.com>
2021-05-21 20:55:26 +0200

    Double equality

--- continuous_integration/install_dependencies.sh
+++ continuous_integration/install_dependencies.sh
@@ -11,18 +11,18 @@ if [[ "$RUNNER_OS" == "Linux" ]]; then
 fi
 
 if [[ "$PYTHON_VERSION" == "3.6" ]]; then
-  python -m pip install scipy=1.1 numpy=1.15 mkl pip pytest lapack ecos scs osqp flake8 cvxopt
+  python -m pip install scipy==1.1 numpy==1.15 mkl pip pytest lapack ecos scs osqp flake8 cvxopt
 elif [[ "$PYTHON_VERSION" == "3.7" ]]; then
-  python -m pip install scipy=1.1 numpy=1.15 mkl pip pytest lapack ecos scs osqp flake8 cvxopt
+  python -m pip install scipy==1.1 numpy==1.15 mkl pip pytest lapack ecos scs osqp flake8 cvxopt
 elif [[ "$PYTHON_VERSION" == "3.8" ]]; then
   # There is a config that works with numpy 1.14, but not 1.15!
   # So we fix things at 1.16.
   # Assuming we use numpy 1.16, the earliest version of scipy we can use is 1.3.
-  python -m pip install scipy=1.3 numpy=1.16 mkl pip pytest lapack ecos scs osqp flake8 cvxopt
+  python -m pip install scipy==1.3 numpy==1.16 mkl pip pytest lapack ecos scs osqp flake8 cvxopt
 elif [[ "$PYTHON_VERSION" == "3.9" ]]; then
   # The earliest version of numpy that works is 1.19.
   # Given numpy 1.19, the earliest version of scipy we can use is 1.5.
-  python -m pip install scipy=1.5 numpy=1.19 mkl pip pytest lapack ecos scs osqp flake8 cvxopt
+  python -m pip install scipy==1.5 numpy==1.19 mkl pip pytest lapack ecos scs osqp flake8 cvxopt
 fi
 
 if [[ "$USE_OPENMP" == "True" ]]; then
