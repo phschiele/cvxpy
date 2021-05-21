683502343aca83c75829f3a6a491b560b4f7f4ba
phschiele <phschiele@gmail.com>
2021-05-21 22:31:50 +0200

    Newer windows sdk

--- continuous_integration/install_dependencies.sh
+++ continuous_integration/install_dependencies.sh
@@ -12,18 +12,18 @@ fi
 
 
 if [[ "$PYTHON_VERSION" == "3.6" ]]; then
-  conda install scipy=1.1 numpy=1.15 pip pytest lapack ecos scs osqp flake8 cvxopt
+  conda install scipy=1.1 numpy=1.15 mkl pip pytest lapack ecos scs osqp flake8 cvxopt
 elif [[ "$PYTHON_VERSION" == "3.7" ]]; then
-  conda install scipy=1.1 numpy=1.15 pip pytest lapack ecos scs osqp flake8 cvxopt
+  conda install scipy=1.1 numpy=1.15 mkl pip pytest lapack ecos scs osqp flake8 cvxopt
 elif [[ "$PYTHON_VERSION" == "3.8" ]]; then
   # There is a config that works with numpy 1.14, but not 1.15!
   # So we fix things at 1.16.
   # Assuming we use numpy 1.16, the earliest version of scipy we can use is 1.3.
-  conda install scipy=1.3 numpy=1.16 pip pytest lapack ecos scs osqp flake8 cvxopt
+  conda install scipy=1.3 numpy=1.16 mkl pip pytest lapack ecos scs osqp flake8 cvxopt
 elif [[ "$PYTHON_VERSION" == "3.9" ]]; then
   # The earliest version of numpy that works is 1.19.
   # Given numpy 1.19, the earliest version of scipy we can use is 1.5.
-  conda install scipy=1.5 numpy=1.19 pip pytest lapack ecos scs osqp flake8 cvxopt
+  conda install scipy=1.5 numpy=1.19 mkl pip pytest lapack ecos scs osqp flake8 cvxopt
 fi
 
 if [[ "$USE_OPENMP" == "True" ]]; then
