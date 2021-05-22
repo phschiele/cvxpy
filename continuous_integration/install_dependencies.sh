540ba883c18bdcb32f14c47d42af6f3bed5f562a
phschiele <phschiele@gmail.com>
2021-05-22 15:15:36 +0200

    Use mkl 2019.04 for python 3.8

--- continuous_integration/install_dependencies.sh
+++ continuous_integration/install_dependencies.sh
@@ -19,7 +19,7 @@ elif [[ "$PYTHON_VERSION" == "3.8" ]]; then
   # There is a config that works with numpy 1.14, but not 1.15!
   # So we fix things at 1.16.
   # Assuming we use numpy 1.16, the earliest version of scipy we can use is 1.3.
-  conda install scipy=1.3 numpy=1.16 mkl=2018.0.3 pip pytest lapack ecos scs osqp flake8 cvxopt
+  conda install scipy=1.3 numpy=1.16 mkl=2019.4 pip pytest lapack ecos scs osqp flake8 cvxopt
 elif [[ "$PYTHON_VERSION" == "3.9" ]]; then
   # The earliest version of numpy that works is 1.19.
   # Given numpy 1.19, the earliest version of scipy we can use is 1.5.
