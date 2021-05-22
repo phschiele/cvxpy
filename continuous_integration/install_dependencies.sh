46c37164377eadf783a1c74f1c09de5dc30aba12
phschiele <phschiele@gmail.com>
2021-05-22 15:03:13 +0200

    Fix mkl version

--- continuous_integration/install_dependencies.sh
+++ continuous_integration/install_dependencies.sh
@@ -12,22 +12,18 @@ fi
 
 
 if [[ "$PYTHON_VERSION" == "3.6" ]]; then
-  conda install pip pytest lapack ecos scs osqp flake8 cvxopt
-  conda install -c conda-forge scipy=1.1 numpy=1.15
+  conda install scipy=1.1 numpy=1.15 mkl=2018.0.3 pip pytest lapack ecos scs osqp flake8 cvxopt
 elif [[ "$PYTHON_VERSION" == "3.7" ]]; then
-  conda install pip pytest lapack ecos scs osqp flake8 cvxopt
-  conda install -c conda-forge scipy=1.1 numpy=1.15
+  conda install scipy=1.1 numpy=1.15 mkl=2018.0.3 pip pytest lapack ecos scs osqp flake8 cvxopt
 elif [[ "$PYTHON_VERSION" == "3.8" ]]; then
   # There is a config that works with numpy 1.14, but not 1.15!
   # So we fix things at 1.16.
   # Assuming we use numpy 1.16, the earliest version of scipy we can use is 1.3.
-  conda install pip pytest lapack ecos scs osqp flake8 cvxopt
-  conda install -c conda-forge scipy=1.3 numpy=1.16
+  conda install scipy=1.3 numpy=1.16 mkl=2018.0.3 pip pytest lapack ecos scs osqp flake8 cvxopt
 elif [[ "$PYTHON_VERSION" == "3.9" ]]; then
   # The earliest version of numpy that works is 1.19.
   # Given numpy 1.19, the earliest version of scipy we can use is 1.5.
-  conda install pip pytest lapack ecos scs osqp flake8 cvxopt
-  conda install -c conda-forge scipy=1.5 numpy=1.19
+  conda install scipy=1.5 numpy=1.19 mkl=2018.0.3 pip pytest lapack ecos scs osqp flake8 cvxopt
 fi
 
 if [[ "$USE_OPENMP" == "True" ]]; then
@@ -39,25 +35,3 @@ python -m pip install diffcp
 if [[ "$COVERAGE" == "True" ]]; then
     python -m pip install coverage coveralls
 fi
-
-python --version
-python -c "import numpy; print('numpy %s' % numpy.__version__)"
-python -c "import scipy; print('scipy %s' % scipy.__version__)"
-
-if [ $USE_OPENMP == "True" ] && [ $RUNNER_OS == "Linux" ]; then
-    CFLAGS="-fopenmp" LDFLAGS="-lgomp" python setup.py install
-    export OMP_NUM_THREADS=4
-else
-    python setup.py install
-fi
-
-python -c "import cvxpy; print(cvxpy.installed_solvers())"
-
-if [[ "$COVERAGE" == "True" ]]; then
-    export WITH_COVERAGE="--with-coverage"
-else
-    export WITH_COVERAGE=""
-fi
-
-pytest $WITH_COVERAGE cvxpy/tests
-pytest $WITH_COVERAGE cvxpy/performance_tests
