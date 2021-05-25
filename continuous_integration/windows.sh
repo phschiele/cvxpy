e217eeff6261da8703508cd90aeb34065db613fc
phschiele <phschiele@gmail.com>
2021-05-25 23:48:08 +0200

    Using bash

---
+++ continuous_integration/windows.sh
@@ -0,0 +1,24 @@
+# Download miniconda
+
+# Get Miniconda and make it the main Python interpreter
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+bash ~/miniconda.sh -b -p ~/miniconda
+export PATH=~/miniconda/bin:$PATH
+
+echo "The PATH environment variable is"
+echo $PATH
+# conda update conda -y
+conda init
+
+conda config --add channels conda-forge
+conda config --add channels oxfordcontrol
+conda create -n testenv --yes python=$env:PYTHON_VERSION mkl=2018.0.3 pip pytest numpy scipy
+conda activate testenv
+conda install --yes lapack ecos scs
+conda install -c anaconda --yes flake8
+python -V
+python3 -V
+pip install diffcp
+pip list
+python3 setup.py install
+pytest cvxpy/tests
