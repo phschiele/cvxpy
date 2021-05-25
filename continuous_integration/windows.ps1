8b3cd022a057a1b5e3b8dbf498f01a35b0fff06d
phschiele <phschiele@gmail.com>
2021-05-25 02:10:20 +0200

    Do not add to path

--- continuous_integration/windows.ps1
+++ continuous_integration/windows.ps1
@@ -15,7 +15,6 @@ Start-Process -Filepath $filepath -ArgumentList $install_args -Wait -Passthru
 
 # Update conda and call "conda init" to handle path management issues on Windows
 
-$env:PATH = "${env:PYTHON};${env:PYTHON}\Scripts;" + $env:PATH
 echo "The PATH environment variable is"
 echo $env:PATH
 # conda update conda -y
