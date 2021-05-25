d9f541df9832e943a825248fe9749f559741b1a0
phschiele <phschiele@gmail.com>
2021-05-25 23:10:48 +0200

    show default python version

--- continuous_integration/windows.ps1
+++ continuous_integration/windows.ps1
@@ -15,6 +15,9 @@ conda create -n testenv --yes python=$env:PYTHON_VERSION mkl=2018.0.3 pip pytest
 conda activate testenv
 conda install --yes lapack ecos scs
 conda install -c anaconda --yes flake8
-pip install diffcp pytest
-python setup.py install
+python -V
+python3 -V
+pip install diffcp
+pip list
+python3 setup.py install
 pytest cvxpy/tests
