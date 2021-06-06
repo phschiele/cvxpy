77bfda36b2d0e20f8101e33c9e029df40abad7f9
phschiele <phschiele@gmail.com>
2021-06-06 20:33:30 +0200

    use coin-or-cbc

--- continuous_integration/install_dependencies.sh
+++ continuous_integration/install_dependencies.sh
@@ -5,38 +5,25 @@
 
 set -e
 
-echo "starting install"
-echo [[ "$RUNNER_OS" == "macOS" ]]
-if [[ "$RUNNER_OS" == "macOS" ]]; then echo "Setting compilers for macOS" ; fi
-if [[ "$RUNNER_OS" == "macOS" ]]; then CC=gcc ; fi
-if [[ "$RUNNER_OS" == "macOS" ]]; then CXX=g++; fi
-
-if [[ "$PYTHON_VERSION" == "3.6" ]]; then
+if [[ "$PYTHON_VERSION" == "3.6" ]] || [[ "$PYTHON_VERSION" == "3.7" ]] || [[ "$PYTHON_VERSION" == "3.8" ]]; then
   conda install scipy=1.3 numpy=1.16 mkl pip pytest lapack ecos scs osqp flake8 cvxopt
-  python -m pip install cplex
-elif [[ "$PYTHON_VERSION" == "3.7" ]]; then
-  conda install scipy=1.3 numpy=1.16 mkl pip pytest lapack ecos scs osqp flake8 cvxopt
-  python -m pip install cplex
-elif [[ "$PYTHON_VERSION" == "3.8" ]]; then
-  # There is a config that works with numpy 1.14, but not 1.15!
-  # So we fix things at 1.16.
-  # Assuming we use numpy 1.16, the earliest version of scipy we can use is 1.3.
-  conda install scipy=1.3 numpy=1.16 mkl pip pytest lapack ecos scs osqp flake8 cvxopt
-  python -m pip install cplex
+  python -m pip install cplex  # CPLEX is not available yet on 3.9
 elif [[ "$PYTHON_VERSION" == "3.9" ]]; then
   # The earliest version of numpy that works is 1.19.
   # Given numpy 1.19, the earliest version of scipy we can use is 1.5.
   conda install scipy=1.5 numpy=1.19 mkl pip pytest lapack ecos scs osqp flake8 cvxopt
 fi
 
-echo [[ "$RUNNER_OS" != "Windows" ]]
+# CBC comes with wheels for windows and needs coin-or-cbc to compile otherwise
+# conda-forge in progress: https://github.com/conda-forge/staged-recipes/pull/14950
 if [[ "$RUNNER_OS" != "Windows" ]]; then
-  echo "$RUNNER_OS"
-  echo "installing coincbc"
-  conda install coincbc
+  conda install coin-or-cbc
+fi
+if [[ "$PYTHON_VERSION" != "3.6" ]] && [[ "$RUNNER_OS" != "Windows" ]]; then
+  python -m pip install cylp
 fi
 
-python -m pip install diffcp gurobipy xpress cylp
+python -m pip install diffcp gurobipy xpress
 
 if [[ "$USE_OPENMP" == "True" ]]; then
   conda install -c conda-forge openmp
