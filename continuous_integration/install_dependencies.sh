cd352edc0c06a5a18c28c9ca97fce86d4b19e043
phschiele <phschiele@gmail.com>
2021-06-06 16:43:54 +0200

    Different setup for windows

--- continuous_integration/install_dependencies.sh
+++ continuous_integration/install_dependencies.sh
@@ -5,35 +5,33 @@
 
 set -e
 
-if [[ "$RUNNER_OS" == "Linux" ]]; then
-    sudo apt-get update -qq
-    sudo apt-get install -qq gfortran libgfortran3
-fi
-
-
 if [[ "$PYTHON_VERSION" == "3.6" ]]; then
   conda install scipy=1.3 numpy=1.16 mkl pip pytest lapack ecos scs osqp flake8 cvxopt coincbc
   python -m pip install cplex
 elif [[ "$PYTHON_VERSION" == "3.7" ]]; then
-  conda install scipy=1.3 numpy=1.16 mkl pip pytest lapack ecos scs osqp flake8 cvxopt coincbc
+  conda install scipy=1.3 numpy=1.16 mkl pip pytest lapack ecos scs osqp flake8 cvxopt
   python -m pip install cplex
 elif [[ "$PYTHON_VERSION" == "3.8" ]]; then
   # There is a config that works with numpy 1.14, but not 1.15!
   # So we fix things at 1.16.
   # Assuming we use numpy 1.16, the earliest version of scipy we can use is 1.3.
-  conda install scipy=1.3 numpy=1.16 mkl pip pytest lapack ecos scs osqp flake8 cvxopt coincbc
+  conda install scipy=1.3 numpy=1.16 mkl pip pytest lapack ecos scs osqp flake8 cvxopt
   python -m pip install cplex
 elif [[ "$PYTHON_VERSION" == "3.9" ]]; then
   # The earliest version of numpy that works is 1.19.
   # Given numpy 1.19, the earliest version of scipy we can use is 1.5.
-  conda install scipy=1.5 numpy=1.19 mkl pip pytest lapack ecos scs osqp flake8 cvxopt coincbc
+  conda install scipy=1.5 numpy=1.19 mkl pip pytest lapack ecos scs osqp flake8 cvxopt
 fi
+
+if [[ "$RUNNER_OS" != "windows"* ]]; then
+    conda install coincbc
+fi
+
+python -m pip install diffcp gurobipy xpress cylp
 
 if [[ "$USE_OPENMP" == "True" ]]; then
     conda install -c conda-forge openmp
 fi
-
-python -m pip install diffcp gurobipy xpress cylp
 
 if [[ "$COVERAGE" == "True" ]]; then
     python -m pip install coverage coveralls
